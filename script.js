7a3bf4704f8f72bcac58b6b2cefb3536518114c0
William Marroush <marroushw@yahoo.com>
2022-01-07 15:38:40 -0500

    finished AddColumns function, and fixed AddRows function

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
 console.log("Hello from script.js!");
+let colnum = 3;
 function createrow(){
     const row = document.createElement("div");
     row.classList.add("row");
-    const col1 = document.createElement("div");
-    const col2 = document.createElement("div");
-    const col3 = document.createElement("div");
-    col1.classList.add("col-sm");
-    col1.classList.add("border");
-    col1.classList.add("border-dark");
-    col1.classList.add("box");
-    col1.classList.add("white");
-    col2.classList.add("col-sm");
-    col2.classList.add("border");
-    col2.classList.add("border-dark");
-    col2.classList.add("box");
-    col2.classList.add("white");
-    col3.classList.add("col-sm");
-    col3.classList.add("border");
-    col3.classList.add("border-dark");
-    col3.classList.add("box");
-    col3.classList.add("white");
-    row.append(col1);
-    row.append(col2);
-    row.append(col3);
+    for (let steps=0;steps<colnum;steps++){
+        const col = document.createElement("div");
+        col.classList.add("col-sm","border","border-dark","box");
+        row.append(col);
+    }
     $(".mt-5").append(row);
+}
+function createcolumn(){
+    colnum++;
+    const newrow = document.createElement("div");
+    newrow.classList.add("col-sm","border","border-dark","box");
+    $('.row').append(newrow);
 }
 $('.add-row').click(function (){
     createrow();
 })
 $('.add-col').click(function(){
     createcolumn();
-})
+});
